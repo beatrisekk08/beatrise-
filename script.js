16de65c954ec4aa61cb921a74bf7c0c2a5414f50
beatrisekk08 <beatrise.klavina@gmail.com>
2025-02-14 10:24:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+alert("hello world");
+
+console.log("wasapppp");
+
+let x = 5;
+let y = 12;
+
+if( x== y){
+    console.log("x ir vienads ar y");
+}
+
+
+
+
+
+
+let number = prompt("enter number: 100");
+if(number > 0)
+{
+    console.log("pozitivs");
+}
+if(number < 0)
+{
+    console.log("negativs");
+}
+if (number == 0)
+{
+    console.log("ir 0")
+}
